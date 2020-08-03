1f932065ba81ffe47adcecd0fd6892e0a7c2167c
cikavladimir <cikavladimir@gmail.com>
2020-08-03 04:59:41 -0400

    Create aliases.sh

---
+++ aliases.sh
@@ -0,0 +1,11 @@
+alias l="echo; ls -lh; echo; pwd"
+alias lt="echo; ls -lrth; echo; pwd"
+alias subl="open -a 'sublime text'"
+alias s="open -a 'sublime text'"
+alias z="source ~/.zshrc"
+alias sz="subl ~/.zshrc"
+alias wh="cd /Users/mac/datum/wh"
+
+export CONDA_HOME=/Users/mac/python/miniconda3
+export PATH=${PATH}:$CONDA_HOME/bin:$CONDA_HOME/lib
+. /Users/mac/python/miniconda3/etc/profile.d/conda.sh
